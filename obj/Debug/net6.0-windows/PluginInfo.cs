3b1aba1d044b11ed76467e9073714819c6aca261
Fox_white <39846845+foxwhite25@users.noreply.github.com>
2023-04-06 19:26:41 +0800

    Delete obj directory

--- obj/Debug/net6.0-windows/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SSLMod
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "SSLMod";
-        public const string PLUGIN_NAME = "SSLMod";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
