756aaafdc7cb2eca2be361577b3836d07dac9609
Fox_white <vct.xie@gmail.com>
2023-04-06 19:30:19 +0800

    Revert "Delete obj directory"
    This reverts commit 3b1aba1d044b11ed76467e9073714819c6aca261.

---
+++ obj/Debug/net472/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace SSLMod
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "SSLMod";
+        public const string PLUGIN_NAME = "SSLMod";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
